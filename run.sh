6dd390d4c4b92eca0681da0ea7dbb3dc857c3c61
Javier de la Rosa <versae@gmail.com>
2020-06-23 08:54:38 +0200

    Updating custom texts and logos

--- run.sh
+++ run.sh
@@ -11,3 +11,10 @@ cp CNAME docs/CNAME
 rm -rf tmp/images
 mv images tmp/
 mv metadata.csv tmp/metadata.csv
+# Custom text and logos
+rpl "PixPlot" "<a href='http://barrxcnn.hdplus.es/'>Barr X Inception CNN</a>" docs/index.html
+rpl "https://dhlab.yale.edu" "https://iarthislab.es/" docs/index.html
+cp iarthis_logo_uma.png docs/assets/images/logo.png
+rpl "<img id='logo' src='assets/images/dhlab-logo.svg' alt='DHLab logo' />" "<img id='logo' src='assets/images/logo.png' alt='iArtHis_Lab' />" docs/index.html
+rpl "Douglas Duhaime" "Javier de la Rosa" docs/index.html
+rpl "Visualizing Image Fields" "Barr X Inception CNN" docs/index.html
